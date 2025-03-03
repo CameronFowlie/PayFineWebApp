f04b3cdcea0ba299fb1de8dbc724e2afaa1b42e4
40312249 <40312249@ads.qub.ac.uk>
2025-03-03 14:32:34 +0000

    Initial commit

---
+++ src/main/java/com/example/payfinewebapp/PayfinewebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.payfinewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayfinewebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayfinewebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/payfinewebapp/PayfinewebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.payfinewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayfinewebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
