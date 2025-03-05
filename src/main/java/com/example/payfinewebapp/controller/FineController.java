c1449ee3b30ce27930ffc8c25f7b9639f1ec51b1
40312249 <40312249@ads.qub.ac.uk>
2025-03-05 10:11:11 +0000

    Updates to hmtl files and controller to resolve thymelead title issues

--- src/main/java/com/example/payfinewebapp/controller/FineController.java
+++ src/main/java/com/example/payfinewebapp/controller/FineController.java
@@ -5,6 +5,8 @@ import com.example.payfinewebapp.repository.FineRepository;
 import com.example.payfinewebapp.service.FineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -14,16 +16,16 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/fines")
-public class FineController
-{
+public class FineController {
+
+
     @Autowired
     private FineService fineService;
     @Autowired
     private FineRepository fineRepo;
 
     @PostMapping
-    public ResponseEntity<Fine> CreateFine(@RequestBody Fine _fine)
-    {
+    public ResponseEntity<Fine> CreateFine(@RequestBody Fine _fine) {
         Fine fine = new Fine();
         fine.setReferenceCode(_fine.getReferenceCode());
         fine.setAmountDue(_fine.getAmountDue());
@@ -37,14 +39,12 @@ public class FineController
 
     /// ///////PROBABLY DELETE LATER/////////////////////////////////////////////////////////
     @GetMapping
-    public List<Fine> getAllFines()
-    {
+    public List<Fine> getAllFines() {
         return fineService.GetAllFines();
     }
 
     @GetMapping("/search")
-    public ResponseEntity<Fine> GetFineByRef(@RequestBody Map<String, String> _fine)
-    {
+    public ResponseEntity<Fine> GetFineByRef(@RequestBody Map<String, String> _fine) {
         //String test = "RCode " + _fine.get("referenceCode") + " Postcode: " + _fine.get("postcode") + " House No: " + _fine.get("houseNo");
 
         Fine fine = fineService.GetFineByReferencePlus(_fine.get("referenceCode"), _fine.get("postcode"), _fine.get("houseNo")).orElseThrow();
@@ -55,22 +55,21 @@ public class FineController
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Fine> GetFineByID(@PathVariable Long id)
-    {
+    public ResponseEntity<Fine> GetFineByID(@PathVariable Long id) {
         Fine fine = fineService.GetFineById(id).orElseThrow();
         return ResponseEntity.ok(fine);
     }
 
     @PutMapping("/{id}")
-    public Fine UpdateFine(@PathVariable Long id, @RequestBody Fine fine)
-    {
+    public Fine UpdateFine(@PathVariable Long id, @RequestBody Fine fine) {
         return fineService.UpdateFine(id, fine);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> DeleteFine(@PathVariable Long id)
-    {
+    public ResponseEntity<Void> DeleteFine(@PathVariable Long id) {
         fineService.DeleteFine(id);
         return ResponseEntity.noContent().build();
     }
+
+
 }
--- src/main/java/com/example/payfinewebapp/web/FineWeb.java
+++ src/main/java/com/example/payfinewebapp/web/FineWeb.java
@@ -14,18 +14,17 @@ import java.util.Optional;
 
 @Controller
 @RequestMapping("paycourtfine")
-public class FineWeb
-{
+public class FineWeb {
+
+
     @Autowired
     private FineService fineService;
 
     Boolean test = false;
 
     @GetMapping
-    public String HomePage()
-    {
-        if (!test)
-        {
+    public String HomePage(Model model) {
+        if (!test) {
             Fine f1 = new Fine("feiwfgw1", 1000.0, LocalDate.of(2020, 1, 8), "BT65 7HU", "23A");
             Fine f2 = new Fine("feiwfgw2", 1000.0, LocalDate.of(2020, 1, 8), "BT65 7HU", "23A");
             Fine f3 = new Fine("feiwfgw3", 1000.0, LocalDate.of(2020, 1, 8), "BT65 7HU", "23A");
@@ -35,36 +34,36 @@ public class FineWeb
             fineService.CreateFine(f3);
             test = true;
         }
+        model.addAttribute("title", "Pay a court fine");
         return "index";
     }
 
     @GetMapping("payonlineenterdetails")
-    public String FindFine(Model model)
-    {
+    public String FindFine(Model model) {
+        model.addAttribute("title", "Enter details");
         model.addAttribute("finerefdto", new FineRefDTO());
         return "enterDetails";
     }
 
+
     @PostMapping
-    public String FindFine(@ModelAttribute FineRefDTO finerefdto)
-    {
+    public String FindFine(@ModelAttribute FineRefDTO finerefdto) {
         String test = "RCode " + finerefdto.getReferenceCode() + " Postcode: " + finerefdto.getPostcode() + " House No: " + finerefdto.getHouseNo();
         System.out.println(test);
         return "redirect:/paycourtfine/paymentscreen/" + finerefdto.getReferenceCode();
     }
 
     @GetMapping("paymentscreen/{ref}")
-    public String PayFine(@PathVariable String ref, Model model)
-    {
+    public String PayFine(@PathVariable String ref, Model model) {
         Optional<Fine> fine = fineService.GetFineByReference(ref);
+        model.addAttribute("title", "Payment details");
         model.addAttribute("fine", fine.get());
         model.addAttribute("paymentdto", new PaymentDTO());
         return "paymentScreen";
     }
 
     @PostMapping("paymentscreen/{ref}")
-    public String FindFine(@PathVariable String ref, @ModelAttribute PaymentDTO paymentdto)
-    {
+    public String FindFine(@PathVariable String ref, @ModelAttribute PaymentDTO paymentdto) {
         String test = "Card Num " + paymentdto.getCardNumber() + " Amount: " + paymentdto.getAmountToPay() + " CVC: " + paymentdto.getCvcNumber();
 
         System.out.println(test);
@@ -73,9 +72,9 @@ public class FineWeb
     }
 
     @GetMapping("paymentconfirmationscreen/{ref}")
-    public String ConfirmFine(@PathVariable String ref, Model model)
-    {
+    public String ConfirmFine(@PathVariable String ref, Model model) {
         Optional<Fine> fine = fineService.GetFineByReference(ref);
+        model.addAttribute("title", "Payment confirmed");
         model.addAttribute("fine", fine.get());
         return "paymentConfirmationScreen";
     }
